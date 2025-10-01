ced2c34f81f406eb9a7fb9693902f7d5da4055ce
adria-sb <adria.sabates@alten.es>
2025-10-01 09:25:02 +0200

    add error handling

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import batteryRoutes from './routes/battery.routes';
+import { errorHandler } from './middleware/errorHandler';
+
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use('/api/battery', batteryRoutes);
+app.use('/api/battery', batteryRoutes, errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
